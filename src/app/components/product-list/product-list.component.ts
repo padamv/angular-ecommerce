a2a50109aa08ebd409977df4befbb0571c35a1ba
padamv <padamv91@gmail.com>
2021-01-07 13:08:08 +0100

    Add pagination to product list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -17,7 +17,7 @@ export class ProductListComponent implements OnInit {
 
   // new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
@@ -96,5 +96,11 @@ export class ProductListComponent implements OnInit {
       this.thePageSize = data.page.size;
       this.theTotalElements = data.page.totalElements;
     };
+  }
+
+  updatePageSize(pageSize: number) {
+    this.thePageSize= pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 }
