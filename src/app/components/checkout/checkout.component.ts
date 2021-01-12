cbe0df8e10b27005c5cdab1851058e32272edb60
padamv <padamv91@gmail.com>
2021-01-12 16:32:27 +0100

    Specify custom validator rule for the customer form controls

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -4,6 +4,7 @@ import { Event } from '@angular/router';
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
+import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -32,10 +33,17 @@ export class CheckoutComponent implements OnInit {
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: new FormControl("", [Validators.required, Validators.minLength(2)]),
-        lastName: new FormControl("", [Validators.required, Validators.minLength(2)]),
+        firstName: new FormControl("", 
+                                [Validators.required, 
+                                Validators.minLength(2), 
+                                Luv2ShopValidators.notOnlyWhitespace]),
+        lastName: new FormControl("", 
+                                [Validators.required, 
+                                Validators.minLength(2), 
+                                Luv2ShopValidators.notOnlyWhitespace]),
         email: new FormControl("", 
-                              [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
+                              [Validators.required, 
+                              Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
       }),
       shippingAddress: this.formBuilder.group({
         street: [""],
