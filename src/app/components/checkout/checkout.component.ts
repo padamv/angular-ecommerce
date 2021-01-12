92269e00828bdf29e16817b58459d60f8294e491
padamv <padamv91@gmail.com>
2021-01-12 16:42:31 +0100

    Specify validation rules for shippingAddress form control

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -46,11 +46,20 @@ export class CheckoutComponent implements OnInit {
                               Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
       }),
       shippingAddress: this.formBuilder.group({
-        street: [""],
-        city: [""],
-        state: [""],
-        country: [""],
-        zipCode: [""]
+        street: new FormControl("", 
+                              [Validators.required, 
+                              Validators.minLength(2), 
+                              Luv2ShopValidators.notOnlyWhitespace]),
+        city: new FormControl("", 
+                            [Validators.required, 
+                            Validators.minLength(2), 
+                            Luv2ShopValidators.notOnlyWhitespace]),
+        state: new FormControl("", [Validators.required]),
+        country: new FormControl("", [Validators.required]),
+        zipCode: new FormControl("", 
+                              [Validators.required, 
+                              Validators.minLength(2), 
+                              Luv2ShopValidators.notOnlyWhitespace])
       }),
       billingAddress: this.formBuilder.group({
         street: [""],
