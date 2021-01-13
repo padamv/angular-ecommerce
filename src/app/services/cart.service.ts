fab4bfd1746a7b3c660fba68cf192d8d3bf42c27
padamv <padamv91@gmail.com>
2021-01-13 14:38:30 +0100

    Change to BehaviorSubject to send the last updatest for every new subscriber

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { temporaryDeclaration } from '@angular/compiler/src/compiler_util/expression_converter';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { CartItem } from '../common/cart-item';
 
 @Injectable({
@@ -10,8 +10,8 @@ export class CartService {
 
   cartItems: CartItem[] = [];
 
-  totalPrice: Subject<number> = new Subject<number>();
-  totalQuantity: Subject<number> = new Subject<number>();
+  totalPrice: Subject<number> = new BehaviorSubject<number>(0);    // 0 initial value
+  totalQuantity: Subject<number> = new BehaviorSubject<number>(0);    // 0 initial value
 
   constructor() { }
 
