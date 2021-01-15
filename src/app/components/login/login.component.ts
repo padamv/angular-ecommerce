3f67ef3bafb7d4fa03cd45b71f837004ddceff37
padamv <padamv91@gmail.com>
2021-01-15 11:04:10 +0100

    Integrate okta sig in widget

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+//import * as OktaSignIn from "@okta/okta-signin-widget";
+const OktaSignIn = require('@okta/okta-signin-widget');
+import myAppConfig from "../../config/my-app-config";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  oktaSignin: any;
+
+  constructor(private oktaAuthService: OktaAuthService) {
+
+    this.oktaSignin = new OktaSignIn({
+      logo: "assets/images/logo.png",
+      baseUrl: myAppConfig.oidc.issuer.split("/oauth2")[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+
+    });
+  }
 
   ngOnInit(): void {
+    this.oktaSignin.remove();
+
+    this.oktaSignin.renderEl({
+      el: "#okta-sign-in-widget"}, // this name should be the same as div tag id in login.component.html
+      (response: any) => {
+        if (response.status === "SUCCESS") {
+          this.oktaAuthService.signInWithRedirect();
+        }
+      },
+      (error: any) => {
+        throw error;
+      }
+    );
   }
 
 }
